ef1ff93039852a97a8fcb75053f81b7e7cb13c77
phywxl <wang-xianliang@sohu.com>
2015-01-29 11:35:27 +0800

    增加jersey支持

---
+++ platform/ttsoft.osgi.test.jersey/src/ttsoft/osgi/test/jersey/StatusResource.java
@@ -0,0 +1,14 @@
+package ttsoft.osgi.test.jersey;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("status")
+public class StatusResource {
+	@GET
+	@Produces("text/plain")
+	public String getStatus() {
+		return "active";
+	}
+}
